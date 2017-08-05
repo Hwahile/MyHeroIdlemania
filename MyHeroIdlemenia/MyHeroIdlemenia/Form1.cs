a40eea7f336bc56f7c994d2dc331fff5b67a86f4
Michael Wittkowski <wenndudasliestbistdugay@gmx.de>
2017-08-05 22:03:24 +0200

    Test

--- MyHeroIdlemenia/MyHeroIdlemenia/Form1.cs
+++ MyHeroIdlemenia/MyHeroIdlemenia/Form1.cs
@@ -10,147 +10,141 @@ using System.Windows.Forms;
 
 namespace MyHeroIdlemenia
 {
-    public partial class Form1 : Form
-    {
-        List<KeyValuePair<int, int>> BenoetigteExp = new List<KeyValuePair<int, int>>();
+	public partial class Form1 : Form
+	{
+		List<KeyValuePair<int, int>> BenoetigteExp = new List<KeyValuePair<int, int>>();
+		Player lPlayer = null;
 
+		
 
+		public Form1()
+		{
+			InitializeComponent();
 
-        //Var und Fomrel für Str
-        double str_str;
-        // test
-        double str_akt;
-        double str_hp;
+			// Anpassungen an der Form
+			button_Str_plus.FlatAppearance.BorderColor = Color.LightCoral;
+			button_Str_plus.FlatAppearance.BorderSize = 1;
 
-        //Var und Fomrel für Dex
-        double dex_dex, dex_cr;
+			button_Dex_plus.FlatAppearance.BorderColor = Color.PaleGreen;
+			button_Dex_plus.FlatAppearance.BorderSize = 1;
 
-        //Var und Fomrel für Con
-        double con_con, con_hp;
+			button_Con_plus.FlatAppearance.BorderColor = Color.PaleGoldenrod;
+			button_Con_plus.FlatAppearance.BorderSize = 1;
 
-        //Var und Fomrel für Int
-        double int_int, int_cd;
+			button_Int_plus.FlatAppearance.BorderColor = Color.LightSkyBlue;
+			button_Int_plus.FlatAppearance.BorderSize = 1;
 
-        //Var Statpoints
-        int statpoints = 250;
+			// BenoetigteExp füllen
+			BenoetigteExp.Add(new KeyValuePair<int, int>(0, 1));
+			BenoetigteExp.Add(new KeyValuePair<int, int>(1, 2));
+			BenoetigteExp.Add(new KeyValuePair<int, int>(2, 5));
+			//BenoetigteExp.Add(new KeyValuePair<int, int>(3, 6));
 
-        //Var für xp
-        int xp = 0, xpsec = 1, lv = 0;
+			lPlayer = new Player("Michi");
+			
+			// DataBindings hinzufügen
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            // BenoetigteExp füllen
-            BenoetigteExp.Add(new KeyValuePair<int, int>(0, 1));
-            BenoetigteExp.Add(new KeyValuePair<int, int>(1, 2));
-            BenoetigteExp.Add(new KeyValuePair<int, int>(2, 5));
-            //BenoetigteExp.Add(new KeyValuePair<int, int>(3, 6));
-            BenoetigteExp.Add(new KeyValuePair<int, int>(-1, -1));
+			textBox_lv.DataBindings.Add("Text", lPlayer, "Level");
+			statPoint_Str.DataBindings.Add("Text", lPlayer, "Strength");
+			statPoint_Dex.DataBindings.Add("Text", lPlayer, "Dexterity");
+			statPoint_Int.DataBindings.Add("Text", lPlayer, "Intelligence");
+			statPoint_Con.DataBindings.Add("Text", lPlayer, "Constitution");
+			textBox_Statuspoints.DataBindings.Add("Text", lPlayer, "Statuspoints");
+			textBox_curXp.DataBindings.Add("Text", lPlayer, "CurrentExp");
+			textBox_Xp_ToUp.DataBindings.Add("Text", lPlayer, "CurrentMaxExp");
+			
 
-        }
+			prgBar_Str.DataBindings.Add("Value", lPlayer, "Strength", true);
+			prgBar_Dex.DataBindings.Add("Value", lPlayer, "Dexterity", true);
+			prgBar_Int.DataBindings.Add("Value", lPlayer, "Intelligence", true);
+			prgBar_Con.DataBindings.Add("Value", lPlayer, "Constitution", true);
+			prgBar_Xp.DataBindings.Add("Value", lPlayer, "CurrentExp", true);
+			prgBar_Xp.DataBindings.Add("Maximum", lPlayer, "CurrentMaxExp", true);
 
-        public Form1()
-        {
-            InitializeComponent();
-            button_Str_plus.FlatAppearance.BorderColor = Color.LightCoral;
-            button_Str_plus.FlatAppearance.BorderSize = 1;
+			lPlayer.CurrentExp = 0;
+			lPlayer.CurrentMaxExp = this.GetCurrentMaxExp();
+		}
 
-            button_Dex_plus.FlatAppearance.BorderColor = Color.PaleGreen;
-            button_Dex_plus.FlatAppearance.BorderSize = 1;
+		private int GetCurrentMaxExp()
+		{
+			KeyValuePair<int, int> AktuellesPair = BenoetigteExp.Where(x => x.Key == lPlayer.Level).FirstOrDefault();
+			return lPlayer.CurrentMaxExp = (AktuellesPair.Value != 0) ? AktuellesPair.Value : Int32.MaxValue;
+		}
 
-            button_Con_plus.FlatAppearance.BorderColor = Color.PaleGoldenrod;
-            button_Con_plus.FlatAppearance.BorderSize = 1;
+		void lvUP()
+		{
+			if (lPlayer.CurrentExp == lPlayer.CurrentMaxExp)
+			{
+				lPlayer.Level++;
+				lPlayer.Statuspoints++;
+				lPlayer.CurrentExp = 0;
+				lPlayer.CurrentMaxExp = this.GetCurrentMaxExp();
+			}
+		}
 
-            button_Int_plus.FlatAppearance.BorderColor = Color.LightSkyBlue;
-            button_Int_plus.FlatAppearance.BorderSize = 1;
+		private void xp_per_sec_Tick(object sender, EventArgs e)
+		{
+			if (lPlayer.ExpPerSecond > prgBar_Xp.Maximum || (prgBar_Xp.Value + lPlayer.ExpPerSecond > prgBar_Xp.Maximum))
+				lPlayer.CurrentExp = lPlayer.CurrentMaxExp;
+			else
+				lPlayer.CurrentExp += lPlayer.ExpPerSecond;
 
-        }
+			lvUP();
+		}
 
-        void lvUP()
-        {
-            if (prgBar_Xp.Value == prgBar_Xp.Maximum)
-            {
-                lv++;
-                textBox_lv.Text = Convert.ToString(lv);
-                statpoints++;
-                textBox_Statuspoints.Text = Convert.ToString(statpoints);
-                textBox_lv.Text = Convert.ToString(lv);
-                prgBar_Xp.Value = 0;
-                //prgBar_Xp.Maximum = prgBar_Xp.Maximum * 2;
-                // LINQ FTW !1111!1
-                KeyValuePair<int, int> AktuellesPair = BenoetigteExp.Where(x => x.Key == lv).FirstOrDefault();
-                prgBar_Xp.Maximum = (AktuellesPair.Value != 0) ? AktuellesPair.Value : Int32.MaxValue;
+		private void btnPlusStat_Click(object sender, EventArgs e)
+		{
+			if (lPlayer.Statuspoints > 0)
+			{
+				Button lButton = sender as Button;
 
-            }
-        }
+				bool lSucces = true;
 
-        private void xp_per_sec_Tick(object sender, EventArgs e)
-        {
-            if (xpsec > prgBar_Xp.Maximum || (prgBar_Xp.Value + xpsec > prgBar_Xp.Maximum))
-            {
-                prgBar_Xp.Value = prgBar_Xp.Maximum;
-            }
-            else
-            {
-                prgBar_Xp.Value = prgBar_Xp.Value + xpsec;
-            }
+				Enum.TryParse<Player.Stat>(lButton.Tag.ToString(), out Player.Stat lStat);
 
-            lvUP();
-            textBox_Xp_ToUp.Text = Convert.ToString(prgBar_Xp.Maximum);
-            textBox_curXp.Text = Convert.ToString(prgBar_Xp.Value);
-        }
+				switch (lStat)
+				{
+					case Player.Stat.Strength:
+						if (lPlayer.Strength < 100)
+							lPlayer.Strength++;
+						else
+							lSucces = false;
+						break;
 
-        private void button_Dex_plus_Click(object sender, EventArgs e)
-        {
-            if (statpoints > 0 && prgBar_Dex.Value < 100)
-            {
-                dex_dex++;
-                dex_cr++;
-                statpoints--;
-                statPoint_Dex.Text = Convert.ToString(dex_dex);
-                textBox_Statuspoints.Text = Convert.ToString(statpoints);
-                prgBar_Dex.Value = Convert.ToInt32(dex_dex);
-            }
-        }
+					case Player.Stat.Dexterity:
+						if (lPlayer.Dexterity < 100)
+						{
+							lPlayer.Dexterity++;
+							if (lPlayer.Dexterity % 5 == 0)
+								lPlayer.CritRate++;
+						}
+						else
+							lSucces = false;
 
-        private void button_Con_plus_Click(object sender, EventArgs e)
-        {
-            if (statpoints > 0 && prgBar_Con.Value < 100)
-            {
-                con_con++;
-                statpoints--;
-                statPoint_Con.Text = Convert.ToString(con_con);
-                textBox_Statuspoints.Text = Convert.ToString(statpoints);
-                prgBar_Con.Value = Convert.ToInt32(con_con);
-            }
-        }
+						break;
 
-        private void button_Int_plus_Click(object sender, EventArgs e)
-        {
-            if (statpoints > 0 && prgBar_Int.Value < 100)
-            {
-                int_int++;
-                statpoints--;
-                statPoint_Int.Text = Convert.ToString(int_int);
-                textBox_Statuspoints.Text = Convert.ToString(statpoints);
-                prgBar_Int.Value = Convert.ToInt32(int_int);
-            }
-        }
+					case Player.Stat.Intelligence:
+						if (lPlayer.Intelligence < 100)
+							lPlayer.Intelligence++;
+						else
+							lSucces = false;
+						break;
 
-        private void button_Str_plus_Click(object sender, EventArgs e)
-        {
-            if (statpoints > 0 && prgBar_Str.Value < 100)
-            {
-                str_str++;
-                statpoints--;
-                statPoint_Str.Text = Convert.ToString(str_str);
-                textBox_Statuspoints.Text = Convert.ToString(statpoints);
-                prgBar_Str.Value = Convert.ToInt32(str_str);
-                prgBar_Str.ForeColor = Color.LightCoral;
-            }
-        }
-    }
+					case Player.Stat.Constitution:
+						if (lPlayer.Constitution < 100)
+							lPlayer.Constitution++;
+						else
+							lSucces = false;
+						break;
 
+					default:
+						break;
+				}
 
+				if (lSucces)
+					lPlayer.Statuspoints--;
+			}
+		}
 
-
+	}
 }
---
+++ MyHeroIdlemenia/MyHeroIdlemenia/Player.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHeroIdlemenia
+{
+	public class Player : INotifyPropertyChanged
+	{
+
+		// test
+		//double str_akt, str_hp, dex_cr, con_hp, int_cd;
+
+		#region Properties (private)
+
+		/// <summary>
+		/// Name des Spielers
+		/// </summary>
+		private string _Name = "";
+
+		/// <summary>
+		/// Erfahrung des Spielers
+		/// </summary>
+		private int _Experience = 0;
+
+		private double _Dexterity = 20.0;
+		private double _Strength = 20.0;
+		private double _Constitution = 20.0;
+		private double _Intelligence = 20.0;
+		private double _CritRate = 0.0;
+
+		private int _Level = 0;
+		private int _ExpPerSecond = 1;
+		private int _Statuspoints = 250;
+		private int _CurrentExp = 0;
+		private int _CurrentMaxExp = 0;
+
+		#endregion Properties (private)
+
+		#region Constructors
+
+		/// <summary>
+		/// Standard Konstruktor
+		/// </summary>
+		public Player(string pName = "")
+		{
+			if (!string.IsNullOrWhiteSpace(pName))
+				this.Name = pName;
+
+		}
+
+		#endregion Constructors
+		
+		#region Properties (public)
+
+		public string Name
+		{
+			get { return this._Name; }
+			set { this._Name = value; }
+		}
+
+		public int Experience
+		{
+			get { return this._Experience; }
+			set { this._Experience = value; }
+		}
+
+		public double Strength
+		{
+			get { return this._Strength; }
+			set
+			{
+				this._Strength = value;
+				NotifyPropertyChanged();
+			}
+		}
+
+		public double Dexterity
+		{
+			get { return this._Dexterity; }
+			set
+			{
+				this._Dexterity = value;
+				NotifyPropertyChanged();
+			}
+		}
+
+		public double Constitution
+		{
+			get { return this._Constitution; }
+			set { this._Constitution = value; NotifyPropertyChanged(); }
+		}
+
+		public double Intelligence
+		{
+			get { return this._Intelligence; }
+			set { this._Intelligence = value; NotifyPropertyChanged(); }
+		}
+
+		public double CritRate
+		{
+			get { return this._CritRate; }
+			set { this._CritRate = value; NotifyPropertyChanged(); }
+		}
+
+		public int Level
+		{
+			get { return this._Level; }
+			set { this._Level = value; NotifyPropertyChanged(); }
+		}
+
+		public int ExpPerSecond
+		{
+			get { return this._ExpPerSecond; }
+			set { this._ExpPerSecond = value; NotifyPropertyChanged(); }
+		}
+
+		public int Statuspoints
+		{
+			get { return this._Statuspoints; }
+			set { this._Statuspoints = value; NotifyPropertyChanged(); }
+		}
+
+		public int CurrentExp
+		{
+			get { return this._CurrentExp; }
+			set { this._CurrentExp = value; NotifyPropertyChanged(); }
+		}
+
+		public int CurrentMaxExp
+		{
+			get { return this._CurrentMaxExp; }
+			set { this._CurrentMaxExp = value; NotifyPropertyChanged(); }
+		}
+
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		// This method is called by the Set accessor of each property.
+		// The CallerMemberName attribute that is applied to the optional propertyName
+		// parameter causes the property name of the caller to be substituted as an argument.
+		private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+		#endregion Properties (public)
+
+
+		/// <summary>
+		/// Enum, für die 4 Hauptwerte
+		/// </summary>
+		public enum Stat
+		{
+			Strength = 0,
+			Dexterity = 1,
+			Constitution = 2,
+			Intelligence = 3,
+		}
+	}
+}
